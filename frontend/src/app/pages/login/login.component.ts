8e6110b8b62c89ed45b92a4b4c6d5fa05a406977
Kaua Martins de Souza <kauams@weg.net>
2026-05-26 11:29:21 -0300

    Connection to database initiated for register

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  usuario = {
+    nome: '',
+    email: '',
+    senha: ''
+  }
+
+  constructor(private http: HttpClient) { }
+
+  criarUsuario() {
+
+    this.http.post(
+      'http://localhost:3000/users',
+      this.usuario
+    )
+      .subscribe({
+        next: (res) => {
+          console.log('Sucesso:', res)
+        },
+
+        error: (err) => {
+          console.log('Erro:', err)
+        }
+      })
+
+  }
 
 }
