9a8f6a51d2039b6852d69bb16020d4f37d2ef512
Kaua Martins de Souza <kauams@weg.net>
2026-05-29 10:38:05 -0300

    Logout funcionadno, e reports tb

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ReportComponent } from './pages/report/report.component';
+import { Component } from '@angular/core';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'report', component: ReportComponent }
 
 ];
